947fdaa68cfddc02ce9bb19db359f7e72c4a4dd3
abc123456 <itboye@163.com>
2019-05-30 17:50:51 +0800

    还款计划 子项

--- app/src/main/java/com/itboye/cardmanage/ui/mine/RepaymentPlanModel.java
+++ app/src/main/java/com/itboye/cardmanage/ui/mine/RepaymentPlanModel.java
@@ -1,11 +1,48 @@
 package com.itboye.cardmanage.ui.mine;
 
 import android.app.Application;
+import android.databinding.ObservableArrayList;
 import android.support.annotation.NonNull;
+import com.itboye.cardmanage.BR;
+import com.itboye.cardmanage.R;
+import com.itboye.cardmanage.interfaces.OnMyItemClickListener;
+import com.itboye.cardmanage.model.LoanModel;
 import me.goldze.mvvmhabit.base.BaseViewModel;
+import me.goldze.mvvmhabit.utils.ToastUtils;
+import me.tatarka.bindingcollectionadapter2.ItemBinding;
+import me.tatarka.bindingcollectionadapter2.OnItemBind;
+import me.tatarka.bindingcollectionadapter2.itembindings.OnItemBindClass;
 
 public class RepaymentPlanModel extends BaseViewModel {
+
+    public ObservableArrayList<LoanModel> planList = new ObservableArrayList<>();
+
     public RepaymentPlanModel(@NonNull Application application) {
         super(application);
+    }
+
+    public OnItemBindClass itemBind2=new OnItemBindClass<>().map(LoanModel.class,new OnItemBind<LoanModel>(){
+
+        @Override
+        public void onItemBind(ItemBinding itemBinding, final int position, LoanModel item) {
+            itemBinding.clearExtras()
+                    .set(BR.item, R.layout.item_repayment_plan)
+                    .bindExtra(BR.listener, new OnMyItemClickListener<LoanModel>() {
+                        @Override
+                        public void onItemClick(int position232323, LoanModel item) {
+                            ToastUtils.showShort(position + "item 点击了吗>>>>>>"+item+"______________"+position232323);
+                        }
+
+                        @Override
+                        public void onLongClick() {
+                            ToastUtils.showShort(position + "item 长按了吗>>>>>>");
+                        }
+                    });
+        }
+    });
+
+    //添加还款计划
+    public void addRepaymentPlan() {
+        planList.add(new LoanModel(getApplication()));
     }
 }
