a3e0537d090c296a5bf10e11cf940495b7266fa9
abc123456 <itboye@163.com>
2019-06-19 17:49:53 +0800

    优化

---
+++ app/src/main/java/com/itboye/cardmanage/model/CardManageModel.java
@@ -0,0 +1,11 @@
+package com.itboye.cardmanage.model;
+
+import android.app.Application;
+import android.support.annotation.NonNull;
+import me.goldze.mvvmhabit.base.BaseViewModel;
+
+public class CardManageModel extends BaseViewModel {
+    public CardManageModel(@NonNull Application application) {
+        super(application);
+    }
+}
--- app/src/main/java/com/itboye/cardmanage/ui/home/PayOrSettlementModel.java
+++ app/src/main/java/com/itboye/cardmanage/ui/home/PayOrSettlementModel.java
@@ -1,18 +1,25 @@
 package com.itboye.cardmanage.ui.home;
 
 import android.app.Application;
+import android.databinding.ObservableArrayList;
 import android.databinding.ObservableField;
+import android.databinding.ObservableList;
 import android.support.annotation.NonNull;
 import android.view.View;
 import android.widget.TextView;
+import com.itboye.cardmanage.BR;
+import com.itboye.cardmanage.R;
+import com.itboye.cardmanage.interfaces.OnMyItemClickListener;
+import com.itboye.cardmanage.model.LoanModel;
 import com.itboye.cardmanage.retrofit.API;
 import com.itboye.cardmanage.retrofit.ApiDisposableObserver;
 import com.itboye.cardmanage.retrofit.AppUtils;
 import com.itboye.cardmanage.retrofit.RetrofitClient;
-import io.reactivex.disposables.Disposable;
-import io.reactivex.functions.Consumer;
 import me.goldze.mvvmhabit.base.BaseViewModel;
 import me.goldze.mvvmhabit.utils.ToastUtils;
+import me.tatarka.bindingcollectionadapter2.ItemBinding;
+import me.tatarka.bindingcollectionadapter2.OnItemBind;
+import me.tatarka.bindingcollectionadapter2.itembindings.OnItemBindClass;
 
 import java.util.ArrayList;
 
@@ -21,11 +28,14 @@ public class PayOrSettlementModel extends BaseViewModel {
     public TextView tab1;
     public TextView tab2;
 
+    public final ObservableList<LoanModel>
+            observableList = new ObservableArrayList<>();
     public ObservableField<Integer> isEmpty = new ObservableField<>(View.GONE);
 
     public PayOrSettlementModel(@NonNull Application application) {
         super(application);
-
+        observableList.add(new LoanModel(application));
+        observableList.add(new LoanModel(application));
     }
 
     //添加卡
@@ -38,6 +48,8 @@ public class PayOrSettlementModel extends BaseViewModel {
             @Override
             public void onResult(Object o, String msg, int code) {
                 ArrayList<String> ar = (ArrayList<String>) o;
+                ar.add("");
+
                 if (ar != null && ar.size() > 0) {
                     isEmpty.set(View.GONE);
                 } else {
@@ -56,4 +68,19 @@ public class PayOrSettlementModel extends BaseViewModel {
             }
         });
     }
+
+
+    public OnItemBindClass itemBinding=new OnItemBindClass<>().map(CardManageModel.class, (itemBinding, position, item) -> itemBinding.clearExtras()
+            .set(BR.item, R.layout.item_loan)
+            .bindExtra(BR.listener, new OnMyItemClickListener<CardManageModel>() {
+                @Override
+                public void onItemClick(int position, CardManageModel item) {
+                    ToastUtils.showShort(position + "item 点击了吗>>>>>>"+item+"______________"+position);
+                }
+
+                @Override
+                public void onLongClick() {
+                    ToastUtils.showShort(position + "item 长按了吗>>>>>>");
+                }
+            }));
 }
